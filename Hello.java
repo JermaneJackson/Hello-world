afdb39ebfde437d5a31510a71dcd775005090271
JermaneJackson <78130387+JermaneJackson@users.noreply.github.com>
2021-01-27 23:16:30 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/*******************************************************************
+* Hello Java
+********************************************************************
+* Jermane Jackson
+* 01/27/21
+* Intro to CS 003
+*******************************************************************/
+
+public class Hello {
+
+	public static void main(String[] args) {
+
+		System.out.println("<your name>");
+		System.out.println("<java project name>");
+		System.out.println("<project discription name>");
+		System.out.println("<version date>");
+		System.out.println("<Course Number and Section>");
+		System.out.println();
+	}
+}
